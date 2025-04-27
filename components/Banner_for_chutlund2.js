1e9a5079655dface3bb44d1a32c73b478d316612
bhol2266 <73753812+bhol2266@users.noreply.github.com>
2025-04-27 23:16:53 +0545

    Update Banner_for_chutlund2.js

--- components/Banner_for_chutlund2.js
+++ components/Banner_for_chutlund2.js
@@ -46,7 +46,7 @@ export default function Banner_for_chutlund2() {
 
         <a href="https://cumcraze1.com" className="underline text-yellow-500 font-inter">cumcraze1.com </a>
         ,
-        <a href="https://milfymadness1.com" className="ml-1 underline text-yellow-500 font-inter">milfymadness1.com</a>
+        <a href="https://cumcraze2.com" className="ml-1 underline text-yellow-500 font-inter">cumcraze2.com</a>
 
       </span>
 
