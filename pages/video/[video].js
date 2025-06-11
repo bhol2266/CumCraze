2282663f633219b67f46e058857d0935487a42eb
ukdevelopers007 <113181134+ukdevelopers007@users.noreply.github.com>
2025-06-11 21:36:59 +0545

    asdfadsf

--- pages/video/[video].js
+++ pages/video/[video].js
@@ -162,14 +162,14 @@ const Videoplayer = () => {
             {preloadedVideoQuality.length > 0 && !noVideo ? (
                 <>
                     <Head>
-                        <title>{`${noVideo ? videoTitleBackup : videoDetails.Title} - ${noVideo ? "720p" : videolinkQualitiesScreenshots.video_qualities_available[videolinkQualitiesScreenshots.video_qualities_available.length - 1].toUpperCase()}`} | CumCraze</title>
-                        <meta name="description" content={`Watch  ${capitalizeFirstLetter(noVideo ? videoTitleBackup : videoDetails.Title)} on CumCraze `} />
+                          <title>{`${videoDetails.Title} | Cumcraze`}</title>
+                        <meta name="description" content={`${capitalizeFirstLetter(noVideo ? videoTitleBackup : videoDetails.Title)} sex video`} />
                         <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
                         <meta name="keywords" content={tagString} />
-                        <meta property="og:title" content={`${noVideo ? videoTitleBackup : videoDetails.Title} - ${noVideo ? "720p" : videolinkQualitiesScreenshots.video_qualities_available[videolinkQualitiesScreenshots.video_qualities_available.length - 1].toUpperCase()}`} />
-                        <meta property="og:description" content={`Watch  ${capitalizeFirstLetter(noVideo ? videoTitleBackup : videoDetails.Title)} on CumCraze `} />
-                        <meta name="twitter:title" content={`${noVideo ? videoTitleBackup : videoDetails.Title} - ${noVideo ? "720p" : videolinkQualitiesScreenshots.video_qualities_available[videolinkQualitiesScreenshots.video_qualities_available.length - 1].toUpperCase()}`} />
-                        <meta name="twitter:description" content={`Watch  ${capitalizeFirstLetter(noVideo ? videoTitleBackup : videoDetails.Title)} on CumCraze `} />
+                        <meta property="og:title" content={`${videoDetails.Title} | Cumcraze`} />
+                        <meta property="og:description" content={`${capitalizeFirstLetter(noVideo ? videoTitleBackup : videoDetails.Title)} sex video`} />
+                        <meta name="twitter:title" content={`${videoDetails.Title} | Cumcraze`} />
+                        <meta name="twitter:description" content={`${capitalizeFirstLetter(noVideo ? videoTitleBackup : videoDetails.Title)} sex video`} />
                         <link rel="canonical" href={`https://www.cumcraze.com/video/${video}`} />
 
                     </Head>
